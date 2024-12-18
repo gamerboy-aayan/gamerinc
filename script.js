44fea0247618d3b061a2533f2247f52de8784608
GamerBoy_Aayan <aayansarkar02@outlook.com>
2024-12-18 18:24:40 +0530

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-// JavaScript to Toggle Hamburger Menu
-document.getElementById("menuToggle").addEventListener("click", function () {
-    const navLinks = document.getElementById("navLinks");
-    navLinks.classList.toggle("active");
-});
